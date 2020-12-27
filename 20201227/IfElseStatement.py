2d03c172edef0627bd189a5e93011b5a126a141d
sandeepa97 <lbsandeepa@gmail.com>
2020-12-27 12:27:57 +0530

    4th commit

---
+++ 20201227/IfElseStatement.py
@@ -0,0 +1,17 @@
+age = 20
+
+if age >= 40 :
+    print("Person is Old")
+else :
+    print("Person is Young")
+
+
+# Example 02
+
+city = "Nugegoda"
+campusName = "NextGen"
+
+if city == "Nugegoda" and campusName == "NextGen" :
+    print("Educated Students")
+else :
+    print("Normal Students")
---
+++ 20201227/IfStatement.py
@@ -0,0 +1,36 @@
+# Example 01
+
+# We have declared and initialed age variable
+age = 20
+
+# Our condition is age greater than or equal to 40, then result should be old
+if age >= 40 :
+    print("This person is old")
+
+
+#Example 02
+
+if age ==40:
+    print("This person is 40")
+
+
+# Example 03
+
+if age < 40:
+    print("This person is young")
+
+# Example 04
+
+city = "Nugegoda"
+campusName = "NextGen"
+
+if city == "Nugegoda" and campusName == "NextGen" :
+    print("Educated Students")
+
+# Example 05
+
+city = "Nugegoda"
+campusName = "NextGen"
+
+if city == "Nugegoda" or campusName == "NextGen" and age < 40  :
+        print("Young Educated Students")
